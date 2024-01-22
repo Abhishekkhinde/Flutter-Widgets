24bcd471ca1a7bc3a914d736247a9b4f7bc5fb5a
Abhishek Khinde <abhisheknkhinde11@gmail.com>
2024-01-22 22:54:01 +0530

    Stateless assignment1

--- assignment_1/lib/main.dart
+++ assignment_1/lib/main.dart
@@ -1,24 +1,23 @@
 import 'package:flutter/material.dart';
 
-void main(){
+void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget{
+class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
-
-  Widget build (BuildContext context){
+  Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: const Text("Core2Web"),
-          ),
-          body: const Center(
-            child: Text("core2web"),
-          ),
-          ),
+        ),
+        body: const Center(
+          child: Text("core2web"),
+        ),
+      ),
     );
   }
 }
