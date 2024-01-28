403411372cde5874f092bfcbc6adf79b46f69c21
Abhishek Khinde <abhisheknkhinde11@gmail.com>
2024-01-28 16:49:59 +0530

    India Flag

---
+++ assignment_14_indianflag/android/app/src/main/kotlin/com/example/assignment_14_indianflag/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.assignment_14_indianflag
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ assignment_14_indianflag/lib/main.dart
@@ -0,0 +1,97 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: 'Indian Flag',
+      home: IndianFlag(),
+    );
+  }
+}
+
+class IndianFlag extends StatefulWidget {
+  const IndianFlag({super.key});
+
+  @override
+  State<IndianFlag> createState() => _IndianFlagState();
+}
+
+class _IndianFlagState extends State<IndianFlag> {
+  int _counter = -1;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Indian Flag"),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => setState(() {
+          _counter++;
+        }),
+      ),
+      body: Center(
+        child: Container(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              (_counter >= 0)
+                  ? Container(
+                      height: 500,
+                      width: 20,
+                      color: Colors.black,
+                    )
+                  : Container(),
+              Column(
+                children: [
+                  (_counter >= 1)
+                      ? Container(
+                          height: 50,
+                          width: 250,
+                          color: Colors.orange,
+                        )
+                      : Container(),
+                  (_counter >= 2)
+                      ? Container(
+                          height: 50,
+                          width: 250,
+                          color: Colors.white,
+                          child: (_counter >= 3)
+                              ? Container(
+                                  height: 50,
+                                  width: 250,
+                                  child: Image.asset("assets/Ashoka.png"),
+                                )
+                              : Container())
+                      : Container(),
+                  (_counter >= 4)
+                      ? Container(
+                          height: 50,
+                          width: 250,
+                          color: Colors.green,
+                        )
+                      : Container()
+                ],
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
