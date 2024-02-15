4df64c8937072ee49b451ddf45d9109fe1a65c19
Abhishek Khinde <abhisheknkhinde11@gmail.com>
2024-02-15 23:17:50 +0530

    Quiz App

--- quizappfinal/lib/main.dart
+++ quizappfinal/lib/main.dart
@@ -9,9 +9,9 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: QuizApp(),
+    return const MaterialApp(
       debugShowCheckedModeBanner: false,
+      home: QuizApp(),
     );
   }
 }
@@ -20,50 +20,248 @@ class QuizApp extends StatefulWidget {
   const QuizApp({super.key});
 
   @override
-  State<QuizApp> createState() => _QuizAppState();
+  State createState() => _QuizAppState();
 }
 
-class _QuizAppState extends State<QuizApp> {
+class _QuizAppState extends State {
   List<Map> allQuestions = [
     {
-      "question": "who is the founder of microsoft",
-      "options": ["Steve Job", "Jeff Bezos", "Bill Gates", "Elone Musk"],
+      "question": "Who is the founder of microoft",
+      "options": ["Steve Jobs", "Jeff Bezos", "Bill Gates", "Elon Musk"],
       "answerIndex": 2,
     },
     {
-      "question": "who is the founder of microsoft",
-      "options": ["Steve Job", "Jeff Bezos", "Bill Gates", "Elone Musk"],
+      "question": "Who is the founder of Apple",
+      "options": ["Steve Jobs", "Jeff Bezos", "Bill Gates", "Elon Musk"],
       "answerIndex": 0,
     },
     {
-      "question": "who is the founder of Amazon",
-      "options": ["Steve Job", "Jeff Bezos", "Bill Gates", "Elone Musk"],
+      "question": "Who is the founder of Amazon",
+      "options": ["Steve Jobs", "Jeff Bezos", "Bill Gates", "Elon Musk"],
       "answerIndex": 1,
     },
     {
-      "question": "who is the founder of Tesla",
-      "options": ["Steve Job", "Jeff Bezos", "Bill Gates", "Elone Musk"],
+      "question": "Who is the founder of tesla",
+      "options": ["Steve Jobs", "Jeff Bezos", "Bill Gates", "Elon Musk"],
       "answerIndex": 3,
     },
     {
-      "question": "who is the founder of Google",
-      "options": ["Steve Job", "Lary Page", "Bill Gates", "Elone Musk"],
+      "question": "Who is the founder of Google",
+      "options": ["Steve Jobs", "Lary Page", "Bill Gates", "Elon Musk"],
       "answerIndex": 1,
-    }
+    },
   ];
 
   bool questionScreen = true;
   int questionIndex = 0;
+  int selectedAnswerIndex = -1;
+  int noOfCurrentAnswer = 0;
 
-  Scaffold questionScreen{
-    if(questionScreen == true){
+  MaterialStateProperty<Color?> checkAnswer(int buttonIndex) {
+    if (selectedAnswerIndex != -1) {
+      if (buttonIndex == allQuestions[questionIndex]["answerIndex"]) {
+        return const MaterialStatePropertyAll(Colors.green);
+      } else if (buttonIndex == selectedAnswerIndex) {
+        return const MaterialStatePropertyAll(Colors.red);
+      } else {
+        return const MaterialStatePropertyAll(null);
+      }
+    } else {
+      return const MaterialStatePropertyAll(null);
+    }
+  }
+
+  void validateCurrentPage() {
+    if (selectedAnswerIndex == -1) {
+      return;
+    }
+
+    if (selectedAnswerIndex == allQuestions[questionIndex]["answerIndex"]) {
+      noOfCurrentAnswer += 1;
+    }
+
+    if (selectedAnswerIndex != -1) {
+      if (questionIndex == allQuestions.length - 1) {
+        setState(() {
+          questionScreen = false;
+        });
+      }
+    }
+
+    selectedAnswerIndex = -1;
+    setState(() {
+      questionIndex++;
+    });
+  }
+
+  Scaffold isQuestionScreen() {
+    if (questionScreen == true) {
       return Scaffold(
         appBar: AppBar(
-          title: const Text("QuizApp",
-          style: TextStyle(
-            fontSize: 30,
-            fontWeight:  FontWeight.w800,
+          title: const Text(
+            "Quiz App",
+            style: TextStyle(
+              fontSize: 30,
+              fontWeight: FontWeight.w800,
+              color: Colors.orange,
+            ),
           ),
+          backgroundColor: Colors.blue,
+          centerTitle: true,
+        ),
+        body: Center(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              const SizedBox(
+                height: 25,
+              ),
+              Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+                const Text(
+                  "Question : ",
+                  style: TextStyle(
+                    fontSize: 25,
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
+                Text(
+                  "${questionIndex + 1}/ ${allQuestions.length}",
+                  style: const TextStyle(
+                    fontSize: 25,
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
+              ]),
+              const SizedBox(
+                height: 25,
+              ),
+              SizedBox(
+                height: 50,
+                width: 380,
+                child: Text(
+                  allQuestions[questionIndex]["question"],
+                  style: const TextStyle(
+                    fontSize: 25,
+                    fontWeight: FontWeight.w400,
+                  ),
+                ),
+              ),
+              const SizedBox(
+                height: 30,
+              ),
+              SizedBox(
+                child: ElevatedButton(
+                  style: ButtonStyle(
+                    backgroundColor: checkAnswer(0),
+                  ),
+                  onPressed: () {
+                    if (selectedAnswerIndex == -1) {
+                      setState(() {
+                        selectedAnswerIndex = 0;
+                      });
+                    }
+                  },
+                  child: Text(
+                    "A . ${allQuestions[questionIndex]["options"][0]}",
+                    style: const TextStyle(
+                      fontSize: 23,
+                      fontWeight: FontWeight.w400,
+                    ),
+                  ),
+                ),
+              ),
+              const SizedBox(
+                height: 30,
+              ),
+              SizedBox(
+                child: ElevatedButton(
+                  style: ButtonStyle(
+                    backgroundColor: checkAnswer(1),
+                  ),
+                  onPressed: () {
+                    if (selectedAnswerIndex == -1) {
+                      setState(() {
+                        selectedAnswerIndex = 1;
+                      });
+                    }
+                  },
+                  child: Text(
+                    "B . ${allQuestions[questionIndex]["options"][1]}",
+                    style: const TextStyle(
+                      fontWeight: FontWeight.w400,
+                      fontSize: 23,
+                    ),
+                  ),
+                ),
+              ),
+              const SizedBox(
+                height: 30,
+              ),
+              SizedBox(
+                child: ElevatedButton(
+                  style: ButtonStyle(
+                    backgroundColor: checkAnswer(2),
+                  ),
+                  onPressed: () {
+                    if (selectedAnswerIndex == -1) {
+                      setState(() {
+                        selectedAnswerIndex = 2;
+                      });
+                    }
+                  },
+                  child: Text(
+                    "C . ${allQuestions[questionIndex]["options"][2]}",
+                    style: const TextStyle(
+                      fontSize: 23,
+                      fontWeight: FontWeight.w400,
+                    ),
+                  ),
+                ),
+              ),
+              const SizedBox(
+                height: 25,
+              ),
+              SizedBox(
+                child: ElevatedButton(
+                  style: ButtonStyle(
+                    backgroundColor: checkAnswer(3),
+                  ),
+                  onPressed: () {
+                    if (selectedAnswerIndex == -1) {
+                      setState(() {
+                        selectedAnswerIndex = 3;
+                      });
+                    }
+                  },
+                  child: Text(
+                    "D . ${allQuestions[questionIndex]["options"][3]}",
+                    style: const TextStyle(
+                      fontSize: 23,
+                      fontWeight: FontWeight.w400,
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            validateCurrentPage();
+          },
+          child: const Icon(Icons.arrow_forward),
+        ),
+      );
+    } else {
+      return Scaffold(
+        appBar: AppBar(
+          title: const Text(
+            "Quiz App",
+            style: TextStyle(
+              fontSize: 30,
+              fontWeight: FontWeight.w800,
+              color: Colors.orange,
+            ),
           ),
           backgroundColor: Colors.blue,
           centerTitle: true,
@@ -71,30 +269,55 @@ class _QuizAppState extends State<QuizApp> {
         body: Column(
           children: [
             const SizedBox(
-              height: 25,
+              height: 20,
             ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                const Text("Quistion :",
+            Image.network(
+              "https://img.freepik.com/premium-vector/winner-trophy-cup-with-ribbon-confetti_51486-122.jpg",
+              height: 500,
+              width: 380,
+              alignment: Alignment.center,
+            ),
+            const Text("Congratulations!!!",
                 style: TextStyle(
                   fontSize: 25,
-                  fontWeight: FontWeight.w600,
-                ),),
-                 Text("${questionIndex+1} / ${allQuestions.length}")
-              ],
-            )
+                  fontWeight: FontWeight.w500,
+                )),
+            const SizedBox(
+              height: 15,
+            ),
+            const Text("You have completed the Quiz",
+                style: TextStyle(
+                  fontSize: 25,
+                  fontWeight: FontWeight.w500,
+                )),
+            Text("$noOfCurrentAnswer/${allQuestions.length}",
+                style: const TextStyle(
+                  fontSize: 25,
+                  fontWeight: FontWeight.w500,
+                )),
+            ElevatedButton(
+                onPressed: () {
+                  questionScreen = true;
+                  questionIndex = 0;
+                  selectedAnswerIndex = -1;
+                  noOfCurrentAnswer = 0;
+                  setState(() {});
+                },
+                child: const Text(
+                  "Reset",
+                  style: TextStyle(
+                    fontWeight: FontWeight.w400,
+                    fontSize: 23,
+                  ),
+                ))
           ],
         ),
       );
     }
-    else{
-      return Scaffold();
-    }
   }
-@override
-Widget build(BuildContext context){
-  return questionScreen();
-}
 
+  @override
+  Widget build(BuildContext context) {
+    return isQuestionScreen();
+  }
 }
